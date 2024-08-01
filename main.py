9b1a618d6d764bba608e2e039b2f9a7dec3c563d
chinmayacm <68702975+chinmayacm@users.noreply.github.com>
2024-08-01 10:21:54 +0530

    Update main.py
    added focus() to guess field upon reset

--- main.py
+++ main.py
@@ -1,99 +1,36 @@
-from pyscript import document
+<!DOCTYPE html>
+<html lang="en">
 
-# from pyodide.ffi.wrappers import add_event_listener
-from pyodide.ffi import create_proxy
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guess the number game</title>
 
-from random import randint
+    <link rel="stylesheet" href="https://pyscript.net/releases/2024.7.1/core.css">
 
+    <script type="module" src="https://pyscript.net/releases/2024.7.1/core.js"></script>
 
-class GuessGame:
-    def __init__(self, min_number, max_number, attempts):
-        self.min_number = min_number
-        self.max_number = max_number
-        self.attempts = attempts
-        self.reset_game()
+    <link rel="stylesheet" href="style.css">
 
-    def reset_game(self):
-        self.attempts_left = self.attempts
-        self.attempts_made = 0
-        self.numbers_entered = []
-        self.random_number = randint(self.min_number, self.max_number)
+    <script type="py" src="main.py"> </script>
+</head>
 
-    def is_game_over(self):
-        return self.attempts_left == 0
+<body>
 
-    def number_submit(self, number):
-        if self.is_game_over():
-            return "You have exhausted your attempts. Reset to play again."
-        self.numbers_entered.append(number)
-        self.attempts_made += 1
-        self.attempts_left -= 1
+    <section class="main-area">
+        <h1>The Guess Game</h1>
 
-        if number == self.random_number:
-            response = f"You guessed right! Congratulations!<br>You guessed in {self.attempts_made} attempts."
-            self.attempts_left = 0
-            return response
-        elif number > self.random_number:
-            response = (
-                f"Your guess is high. You have {self.attempts_left} attempts left."
-            )
-        else:  # number < self.random_number
-            response = (
-                f"Your guess is low. You have {self.attempts_left} attempts left."
-            )
+        <label for="guess" class="guess-label">Guess a number between 1 and 10.</label>
+        <input type="number" id="guess" max="10" min="1" required pattern="([1-9]|10)?"/>
+        <button id="submit">Submit</button>
+        <button id="reset">Reset</button>
 
-        if self.is_game_over():
-            response += (
-                "<br>The random number was "
-                + str(self.random_number)
-                + ". Reset to play again."
-            )
+        <p id="message">Make a guess!</p>
+        <p id="sofar">Your guesses so far: </br> <span id="guessList">[None]</span></p>
+        <p id="at-left">Attempts Left: <span id="attemptsLeft">4/4</p>
 
-        return response
+    </section>
 
+</body>
 
-game = GuessGame(1, 10, 4)
-
-
-def submit_guess(*args):
-    guess = document.querySelector("#guess").value
-
-    try:
-        guess = int(guess)
-        if 1 <= guess <= 10:
-            result = game.number_submit(guess)
-            document.querySelector("#message").innerHTML = result
-            attempts_left = game.attempts_left
-            document.querySelector("#attemptsLeft").textContent = (
-                str(attempts_left) + "/" + str(game.attempts)
-            )
-            numbers_entered = game.numbers_entered
-            document.querySelector("#guessList").textContent = str(numbers_entered)
-            document.querySelector("#guess").value = ""
-        else:
-            document.querySelector("#message").textContent = (
-                "Please enter a number between 1 and 10."
-            )
-    except Exception:
-        document.querySelector("#message").textContent = (
-            "Please enter a number between 1 and 10."
-        )
-
-
-#     document.querySelector(".log p").textContent = game.random_number
-
-
-def reset(*args):
-    game.reset_game()
-    document.querySelector("#message").textContent = "Game has been reset!"
-    document.querySelector("#guessList").textContent = "[None]"
-    document.querySelector("#guess").value = ""
-
-
-submit_btn = document.querySelector("#submit")
-# add_event_listener(submit_btn,'click',submit_guess)
-submit_btn.addEventListener("click", create_proxy(submit_guess))
-
-reset_btn = document.querySelector("#reset")
-# add_event_listener(reset_btn,'click',reset)
-reset_btn.addEventListener("click", create_proxy(reset))
+</html>
